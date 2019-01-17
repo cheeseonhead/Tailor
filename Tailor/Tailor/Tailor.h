fc5ef27121cf88bd288b028f7c8a6b5bda975c8d
Jeffrey Wu <mengyang.developer@gmail.com>
2019-01-16 20:29:40 -0500

    Created project files

---
+++ Tailor/Tailor/Tailor.h
@@ -0,0 +1,19 @@
+//
+//  Tailor.h
+//  Tailor
+//
+//  Created by Jeffrey Wu on 2019-01-16.
+//  Copyright © 2019 cheeseonhead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tailor.
+FOUNDATION_EXPORT double TailorVersionNumber;
+
+//! Project version string for Tailor.
+FOUNDATION_EXPORT const unsigned char TailorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tailor/PublicHeader.h>
+
+
